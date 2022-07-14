751880ca0a1ed4830baac48d403ff148df785e6b
Shahid khan <khansahab3102@gmail.com>
2022-07-14 11:47:17 +0530

    changed a little bit for result duplication problem

--- src/DuplicateInArray.java
+++ src/DuplicateInArray.java
@@ -1,7 +1,10 @@
 //to check duplicate element in the array
+import java.lang.reflect.Array;
+import java.util.*;
 public class DuplicateInArray {
     public static void main(String[] args) {
-        int arr[]={1,2,4,5,5,6,77,7,7,8,9,9};
+        int arr[]={1,2,4,5,4,4,4,4,5,5,6,77,7,8,8,8,88,7,8,9,9};
+        Arrays.sort(arr);
         int pre=0;
         for (int i = 0; i < arr.length; i++) {
             int a=arr[i];
